b49d44eaaead1c7646fc388672bb113d38f708a4
Muhammad Faishal Rizqi Pratama <rizqifaishal03@gmail.com>
2025-06-12 05:28:47 +0700

    initial commit

---
+++ app/src/main/java/com/muhammadfaishalrizqipratama0094/cookit_/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.muhammadfaishalrizqipratama0094.cookit_
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.muhammadfaishalrizqipratama0094.cookit_.ui.theme.CookIT_Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            CookIT_Theme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    CookIT_Theme {
+        Greeting("Android")
+    }
+}
