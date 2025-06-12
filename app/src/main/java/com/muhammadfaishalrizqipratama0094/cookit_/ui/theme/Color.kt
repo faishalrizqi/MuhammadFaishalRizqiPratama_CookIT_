03e65edf0f09160a230c71f1e61faeaa7f00dd15
Muhammad Faishal Rizqi Pratama <rizqifaishal03@gmail.com>
2025-06-12 10:00:16 +0700

    merubah warna topApp bar dan tombol-tombol

--- app/src/main/java/com/muhammadfaishalrizqipratama0094/cookit_/ui/theme/Color.kt
+++ app/src/main/java/com/muhammadfaishalrizqipratama0094/cookit_/ui/theme/Color.kt
@@ -2,6 +2,14 @@ package com.muhammadfaishalrizqipratama0094.cookit_.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
+val Orange_Primary = Color(0xFFFF9800)
+val Orange_Light = Color(0xFFFFC947)
+val Orange_Dark = Color(0xFFC66900)
+
+val Orange_Secondary = Color(0xFFFFC107)
+val Orange_Secondary_Light = Color(0xFFFFF350)
+val Orange_Secondary_Dark = Color(0xFFC79100)
+
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
